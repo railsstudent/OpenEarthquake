2bbd32aecd4b4880fa1b414c8860b8e8c6c30de9
Connie <cleung334@gmail.com>
2014-03-29 11:45:07 +0800

    package renamed.

--- src/com/blueskyconnie/openearthquake/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.blueskyconnie.openearthquake;
-
-public class Constants {
-
-	public static final String EARTHQUAKE_INFO_KEY = "earthquake_info_key";
-	public static final String EARTHQUAKE_TITLE = "earthquake_info_key";
-	public static final String EARTHQUAKE_REST_URL = "earthquake_rest_url";
-}
