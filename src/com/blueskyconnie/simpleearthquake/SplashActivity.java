5193c319aa631cb904fc9b17e3eab5b0a5e7f3b6
Connie <cleung334@gmail.com>
2014-03-30 13:34:43 +0800

    Inherit RoboActivity instead of RoboSplashActivity. Because alertdialog cannot called in non-UI thread in startNextACtivity method.

--- src/com/blueskyconnie/simpleearthquake/SplashActivity.java
+++ src/com/blueskyconnie/simpleearthquake/SplashActivity.java
@@ -1,29 +1,46 @@
 package com.blueskyconnie.simpleearthquake;
 
-import roboguice.activity.RoboSplashActivity;
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
-import com.blueskyconnie.simpleearthquake.R;
 import com.blueskyconnie.simpleearthquake.helper.AlertDialogHelper;
 import com.blueskyconnie.simpleearthquake.helper.ConnectionDetector;
 
-public class SplashActivity extends RoboSplashActivity {
+@ContentView(R.layout.activity_splash)
+public class SplashActivity extends RoboActivity {
 
+	private static final String TAG = "SplashActivity";
+	private static final int DELAY_SECONDS = (int) 2.5 * 1000;
+
+	private ConnectionDetector detector;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		this.setContentView(R.layout.activity_splash);
+		detector = new ConnectionDetector(this);
+		if (!detector.isConnectingToInternet()) {
+			AlertDialogHelper.showNoInternetDialog(this);
+			Log.i(TAG, "No Internet Connection. Quit now.");
+		} else {
+			final Handler handler = new Handler();
+			handler.postDelayed(new Runnable() {
+				public void run() {
+					startNextActivity();	
+					Log.i(TAG, "Launch next activity");
+				}
+			}, DELAY_SECONDS);
+			
+		}
 	}
 
-	@Override
 	protected void startNextActivity() {
-		ConnectionDetector detector = new ConnectionDetector(this);
 		if (detector.isConnectingToInternet()) {
 			startActivity(new Intent(this, MainActivity.class));
-		} else {
-			 AlertDialogHelper.showNoInternetDialog(this);
+			finish();
 		}
-		
 	}
 }
