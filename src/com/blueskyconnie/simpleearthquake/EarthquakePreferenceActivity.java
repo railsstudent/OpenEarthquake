756f741ca6d185cef04d378c61f225fdd8d57be3
Connie <cleung334@gmail.com>
2014-04-22 00:38:51 +0800

    Add up navigation in earthquake preference activity.

--- src/com/blueskyconnie/simpleearthquake/EarthquakePreferenceActivity.java
+++ src/com/blueskyconnie/simpleearthquake/EarthquakePreferenceActivity.java
@@ -1,11 +1,17 @@
 package com.blueskyconnie.simpleearthquake;
 
+import com.blueskyconnie.simpleearthquake.base.RoboActionBarActivity;
+
 import roboguice.activity.RoboFragmentActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
-public class EarthquakePreferenceActivity extends RoboFragmentActivity  {
+public class EarthquakePreferenceActivity extends RoboActionBarActivity  {
 
+	private static final String TAG = "EarthquakePreferenceActivity";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -14,5 +20,18 @@ public class EarthquakePreferenceActivity extends RoboFragmentActivity  {
 			getSupportFragmentManager().beginTransaction()
 					.add(android.R.id.content, new EarthquakePreferenceFragment()).commit();
 		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		if (item.getItemId() == android.R.id.home) {
+			Intent upIntent = getSupportParentActivityIntent();
+	    	upIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+	    	this.supportNavigateUpTo(upIntent);
+	    	Log.i(TAG, "android.R.id.home: Up navigation");
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 }
